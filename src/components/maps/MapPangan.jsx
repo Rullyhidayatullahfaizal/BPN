40767c8dab77171ee6d2c742125b7f0ec35d8943
Rully_H-F <Rullyhidayatullahfaizal@gmail.com>
2024-05-15 00:06:38 +0700

    update peta,charts

--- src/components/maps/MapPangan.jsx
+++ src/components/maps/MapPangan.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
-import { MapContainer, GeoJSON, Tooltip } from "react-leaflet";
+import React, { useState } from "react";
+import { MapContainer, GeoJSON, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import "./MapPangan.css"
+import "./MapPangan.css";
+import { BarBpn } from "../statistik/BarsBpn";
+import { RowBpn } from "../statistik/RowBpn";
 
 export const MapPangan = ({ countries }) => {
   const [selectedProvince, setSelectedProvince] = useState(null);
-
+  const [modalVisible, setModalVisible] = useState(false);
+  
   const mapStyle = {
     fillColor: "green",
-    weight: 1,
+    weight: 2,
     color: "yellow",
     fillOpacity: 1,
   };
@@ -17,62 +20,119 @@ export const MapPangan = ({ countries }) => {
     layer.on({
       mouseover: (e) => highlightFeature(e),
       mouseout: (e) => resetHighlight(e),
-      click: () => handleFeatureClick(feature),
+      click: () => {
+        setSelectedProvince(feature.properties.Propinsi);
+        setModalVisible(true);
+      },
     });
 
-    // Bind a tooltip to each feature
     layer.bindTooltip(feature.properties.Propinsi, {
       permanent: false,
       direction: "top",
-      className: "custom-tooltip"
     });
+
+    // layer.bindTooltip(feature.properties.Propinsi, {
+    //   permanent: true,
+    //   direction: "center",
+    //   className: "province-label", // Add a custom class for styling
+    // });
   };
 
-  // Function to highlight feature on hover
   const highlightFeature = (e) => {
     const layer = e.target;
     layer.setStyle({
       weight: 5,
-      color: '#666',
-      dashArray: '',
-      fillOpacity: 0.7
+      color: "#666",
+      dashArray: "",
+      fillOpacity: 0.7,
     });
   };
 
-  // Function to reset highlight when not hovered
   const resetHighlight = (e) => {
     const layer = e.target;
-    layer.setStyle({
-      weight: 2,
-      color: 'red',
-      fillOpacity: 1,
-    });
+    layer.setStyle(mapStyle); // Use original style defined in mapStyle
   };
 
-  // Function to handle feature click and show modal
-  const handleFeatureClick = (feature) => {
-    setSelectedProvince(feature.properties.Propinsi);
-  };
+  console.log(countries)
 
+  // const provinceCapitals = countries.Features.map(feature => {
+  //   // Flattening the coordinates array to find a central point
+  //   const coordinates = feature.geometry.coordinates.flat(Infinity);
+  //   const capitalCoordinates = coordinates.length > 0 ? coordinates[0] : [0, 0];
+  //   return {
+  //     name: feature.properties.Propinsi,
+  //     coordinates: [capitalCoordinates[1], capitalCoordinates[0]]
+  //   };
+  // });
+  
   return (
-    <div className='mt-10'>
-      <div>
-        <h1 className='text-center text-4xl font-bold text-green_three mb-10'>Pangan Indonesia</h1>
-      </div>
+    <>
       <MapContainer style={{ height: "90vh" }} zoom={5} center={[-2.5, 118]}>
-        <GeoJSON data={countries} style={mapStyle} onEachFeature={onEachFeature}></GeoJSON>
+        <GeoJSON
+          data={countries}
+          style={mapStyle}
+          onEachFeature={onEachFeature}
+        ></GeoJSON>
+        {/* {provinceCapitals.map((province, idx) => (
+          <Marker key={idx} position={province.coordinates}>
+            <Popup>{properties.Propinsi}</Popup>
+          </Marker>
+        ))} */}
       </MapContainer>
-      {selectedProvince && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white p-5 rounded-lg shadow-lg">
-            <button className="absolute top-0 right-0 mt-2 mr-2" onClick={() => setSelectedProvince(null)}>
-              <span className="text-gray-600 hover:text-gray-900 text-2xl">&times;</span>
-            </button>
-            <h2 className="text-xl font-bold mb-4">{selectedProvince}</h2>
-            <p>Informasi lebih lanjut tentang {selectedProvince}.</p>
+
+      {modalVisible && (
+        <div
+          id="static-modal"
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-[1000]"
+        >
+          <div className="relative p-4 w-full max-w-2xl max-h-full">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <div className="flex justify-between items-center p-4 md:p-5 border-b rounded-t bg-green_two dark:border-gray-600">
+                <div className="flex-1 flex justify-center">
+                  <h3 className="text-xl font-bold text-gray-900 dark:text-white">
+                    STATISTIK {selectedProvince}
+                  </h3>
+                </div>
+                <button
+                  type="button"
+                  className="text-yellow bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  onClick={() => setModalVisible(false)}
+                >
+                  <svg
+                    className="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+              <div className="p-4 md:p-2 space-y-0">
+                <BarBpn></BarBpn>
+                <RowBpn></RowBpn>
+              </div>
+              <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button
+                  type="button"
+                  className="py-2.5 px-5 ms-3 text-sm font-medium text-white focus:outline-none bg-green_three rounded-lg border border-gray-200 hover:bg-green_one hover:text-red-500 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                  onClick={() => setModalVisible(false)}
+                >
+                  Tutup
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
